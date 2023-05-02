329940902e1f66bd4c98a6b0a0d0725cce52710f
marciovz <marciovz@yahoo.com.br>
2023-05-02 14:40:52 -0300

    Adicionado arquivo de configuração do jest

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -2,33 +2,35 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile';
 
+describe('Profile Screen', () => {
+  it('should have placeholder correctly in user name input', async () => {
+    const { getByPlaceholderText } = render(<Profile />)
+  
+    const inputName = getByPlaceholderText('Nome');
+  
+    expect(inputName).toBeTruthy();
+    expect(inputName.props.placeholder).toBeTruthy();
+    expect(inputName.props.placeholder).toBe('Nome');
+  });
 
-test('checks if user data has been loaded', () => {
-  const { getByTestId } = render(<Profile />);
-
-  const inputName = getByTestId('input-name');
-  const inputSurname = getByTestId('input-surname');
-
-  expect(inputName.props.value).toEqual('Rodrigo');
-  expect(inputSurname.props.value).toEqual('Gonçalves');
+  it('should be load user data', () => {
+    const { getByTestId } = render(<Profile />);
+  
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
+  
+    expect(inputName.props.value).toEqual('Rodrigo');
+    expect(inputSurname.props.value).toEqual('Gonçalves');
+  });
+  
+  it('should exist title correctly', () => {
+    const { getByTestId } = render(<Profile />);
+  
+    const textTitle = getByTestId('text-title');
+  
+    expect(textTitle.props.children).toContain('Perfil');
+  });
+  
+  
 })
 
-test('checks if title render correctly', () => {
-  const { getByTestId } = render(<Profile />);
-
-  const textTitle = getByTestId('text-title');
-
-  expect(textTitle.props.children).toContain('Perfil');
-
-});
-
-test('check if show correctly user input name placeholder', async () => {
-  const { getByPlaceholderText } = render(<Profile />)
-
-  const inputName = getByPlaceholderText('Nome');
-
-  expect(inputName).toBeTruthy();
-  expect(inputName.props.placeholder).toBeTruthy();
-  expect(inputName.props.placeholder).toBe('Nome');
-
-});
